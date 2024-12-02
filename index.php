e50203fe1c31036624962bfab5198ca752c7f10d
FarisaCA <chennoth786@gmail.com>
2024-12-02 17:40:29 +0530

    done

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Airline Reservation System</title>
-    <link rel="stylesheet" href="helo-style.css"> 
+    <link rel="stylesheet" href="../helo-style.css"> 
     <style>
         .options-container{
             border: 2px solid #005eb8;
